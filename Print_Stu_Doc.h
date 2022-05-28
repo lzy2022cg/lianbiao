5aba8a4be8a278459e244d347147cdaee33da60a
一条大黄鱼 <101653801+lzy2022cg@users.noreply.github.com>
2022-05-28 22:58:15 +0800

    Update Print_Stu_Doc.h

--- Print_Stu_Doc.h
+++ Print_Stu_Doc.h
@@ -1,12 +1,13 @@
-void Print_Stu_Doc(struct stud_node* head)
+void Print_Stu_Doc(List head)
 {
-	struct stud_node* ptr;
+	List ptr;
 	if (head == NULL) {
-		printf(" \nNo Records \n");
+		printf("\nNo Records \n");
 		return;
 	}
-	printf(" \nThe Students'Records Are:\n");
-	printf("Num \t Name \t Score \n");
-	for (ptr = head; ptr != NULL; ptr = ptr->next)
+	printf("\nThe student's Records Are: \n");
+	printf("Num\t Name \t Score\n");
+	for (ptr = head; ptr->next != NULL; ptr = ptr->next) {
 		printf("%d \t%s \t%d\n", ptr->num, ptr->name, ptr->score);
+	}
 }
