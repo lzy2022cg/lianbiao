5622c2d6ff591827c4480e0f63d2f77d77dbb931
一条大黄鱼 <101653801+lzy2022cg@users.noreply.github.com>
2022-05-28 22:54:16 +0800

    Update Create_Stu_Doc.h

--- Create_Stu_Doc.h
+++ Create_Stu_Doc.h
@@ -1,19 +1,20 @@
-struct stud_node* Create_Stu_Doc()
+List Create_Stu_Doc()
 {
-	struct stud_node* head, * p;
+	List head, p;
 	int num, score;
 	char name[20];
-	int size = sizeof(struct stud_node*);
+	int size = sizeof(struct stud_node);
 
 	head = NULL;
-	printf("Input num,name and score:\n");
+	printf("Input num,name and score: ");
 	scanf("%d%s%d", &num, name, &score);
 	while (num != 0) {
-		p = (struct stud_node*)malloc(size);
+		p = (List)malloc(size);
 		p->num = num;
 		strcpy(p->name, name);
 		p->score = score;
 		head = InsertDoc(head, p);
+		printf("Input num,name and score: ");
 		scanf("%d%s%d", &num, name, &score);
 	}
 	return head;
