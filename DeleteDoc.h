89101eb4e1defb4816ec309e76fbb3b9b73446cf
一条大黄鱼 <101653801+lzy2022cg@users.noreply.github.com>
2022-05-28 22:57:46 +0800

    Update DeleteDoc.h

--- DeleteDoc.h
+++ DeleteDoc.h
@@ -1,26 +1,25 @@
-struct stud_node* DeleteDoc(struct stud_node* head, int num)
+List DeleteDoc(List head, int i)
 {
-	struct stud_node* ptr1, * ptr2;
+	List ptr1, ptr2;
 
-	if (head == NULL)
-		return NULL;          /*判断是否为空链表*/
-	else if (head->num == num) {    /*判断要删除的是否为表头*/
-		ptr2 = head;
+	if (head == NULL) {
+		return NULL;
+	}
+	else if (head->num == i) {
 		head = head->next;
-		free(ptr2);
-		return head;
 	}
 	else {
 		ptr1 = head;
 		ptr2 = head->next;
 		while (ptr2 != NULL) {
-			if (ptr2->num == num) {
+			if (ptr2->num == i) {
 				ptr1->next = ptr2->next;
 				free(ptr2);
 			}
-			else
+			else {
 				ptr1 = ptr2;
-			ptr2 = ptr1->next;
+				ptr2 = ptr2->next;
+			}
 		}
 	}
 	return head;
